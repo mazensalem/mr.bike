f879966a1a4a9de94f10bc6509713acf3c26bb05
mazensalem <salemmazen27@gmail.com>
2021-05-09 00:36:11 +0200

    Make the home page

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      height: {
+        100: "27rem",
+      },
+      margin: {
+        fifty: "50%",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
